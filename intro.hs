ee318f2ce555449c04673a772f289d8c8bada90f
dinnu93 <dinesh.padisetti@gmail.com>
2016-02-26 01:50:50 +0530

    Added some more code

--- intro.hs
+++ intro.hs
@@ -25,6 +25,33 @@ rightTriangles :: [(Int, Int, Int)]
 rightTriangles = [(z,y,x) | x <- [1..10], y <- [1..x],z <- [1..y], y^2 + z^2 == x^2, x + y + z == 24]
 
 factorial :: Integer -> Integer
-factorial n = if n == 0
-                 then 1
-                 else n * (factorial (n-1))
+factorial 0 =  1
+factorial n = n * (factorial (n-1))
+
+sumtorial :: Integer -> Integer
+sumtorial 0 = 0
+sumtorial n = n + sumtorial (n-1)
+
+hailstone :: Integer -> Integer
+hailstone n
+  | mod n 2 == 0 = div n 2
+  | otherwise = 3*n + 1
+
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone n)
+
+
+intListLength :: [Integer] -> Integer
+intListLength [] = 0
+intListLength (_:xs) = 1 + intListLength xs
+
+sumEveryTwo :: [Integer] -> [Integer]
+sumEveryTwo [] = []
+sumEveryTwo (x:[]) = [x]
+sumEveryTwo (x:(y:zs)) = (x+y) : sumEveryTwo zs
+
+hailstoneLen :: Integer -> Integer
+hailstoneLen n = intListLength (hailstoneSeq n) - 1
+
+
