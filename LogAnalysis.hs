a2a4f846f44ff2ddb49ccd12b93625d2e1b726db
dinnu93 <dinesh.padisetti@gmail.com>
2016-03-07 13:39:53 +0530

    Deleted a bunch of files

--- LogAnalysis.hs
+++
@@ -1,7 +0,0 @@
-module LogAnalysis where
-import Log
-
-parseMessage :: String -> LogMessage
-parseMessage s
-  | ls !! 0 /= 1  = Unknown s
-  where ls = [x | x <- [1..((length s)-1)], s !! x == ' ']
--- intro.hs
+++ intro.hs
@@ -83,7 +83,9 @@ quicksort (x:xs) =
   let smallSorted = quicksort [a | a <- xs, a <= x]
       bigSorted = quicksort [a | a <- xs, a > x]
   in smallSorted ++ [x] ++ bigSorted
-  
+
+
+
 -- Look Up for Collatz Conjecture
 hailstone :: Integer -> Integer
 hailstone n
@@ -106,42 +108,6 @@ sumEveryTwo (x:(y:zs)) = (x+y) : sumEveryTwo zs
 
 hailstoneLen :: Integer -> Integer
 hailstoneLen n = intListLength (hailstoneSeq n) - 1
-
--- Exercise Problem Solutions of cis194 first chapter
-
--- Credit Card Validator
-
-toDigits :: Integer -> [Integer]
-toDigits n
-  | n <= 0 = []
-  | otherwise = toDigits (quot n 10) ++ [(mod n 10)]
-
-toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = reverse (toDigits n)
-    
-lastSecond :: [a] -> a
-lastSecond ls = head (drop ((length ls)-2) ls)
-
-doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther [] = []
-doubleEveryOther (x:[]) = [x]
-doubleEveryOther zs = (doubleEveryOther (take ((length zs)-2) zs)) ++ [2*(lastSecond zs), (last zs)]
-
-sumDigits :: [Integer] -> Integer
-sumDigits [] = 0
-sumDigits (x:xs) = (sum (toDigits x)) + (sumDigits xs)
-
-validate :: Integer -> Bool
-validate n = (mod (sumDigits (doubleEveryOther (toDigits n))) 10) == 0
-
--- Towers of Hanoi Problem
-
-type Peg = String
-type Move = (Peg, Peg)
-
-hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 1 first last temp = [(first,last)]
-hanoi n first last temp = (hanoi (n-1) first temp last) ++ (hanoi 1 first last temp) ++ (hanoi (n-1) temp last first)
  
 -- Algebraic Data Types : Chapter-2
 
