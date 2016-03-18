8a495768a477e740f4851c810acfe91fbf67fdda
dinnu93 <dinesh.padisetti@gmail.com>
2016-03-18 09:33:56 +0530

    Added Merge Sort Algorithm

--- algo.hs
+++ algo.hs
@@ -11,9 +11,28 @@ insert x [] = [x]
 insert x (y:ys)
           | x < y = x : y : ys
           | otherwise = y : insert x ys
-            
-  
 
+-- Merge Sort
+
+-- Merge step for two sorted lists
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ls = ls
+merge ls [] = ls
+merge xs@(x:xss) ys@(y:yss)
+  | x < y = x : merge xss ys
+  | otherwise = y : merge xs yss
+
+-- Sort Step
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort ls = merge (mergeSort firstHalf) (mergeSort secondHalf)
+  where halfLen = ceiling $ (fromIntegral $ length ls)/2
+        firstHalf = map (ls !!) [0..halfLen-1]
+        secondHalf = map (ls !!) [halfLen..(length ls)-1]
+
+        
 -- Quick Sort
 
 quickSort :: Ord a => [a] -> [a]
