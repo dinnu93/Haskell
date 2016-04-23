12c461f7e9caeb0f362bd8c816a3b6a1445c9d95
dinnu93 <dinesh.padisetti@gmail.com>
2016-04-23 23:45:57 +0530

    Modified wc

--- wc.hs
+++ wc.hs
@@ -3,7 +3,7 @@ import System.Environment
 main = do
   args <- getArgs
   file <- readFile $ args !! 0
-  putStr $ (show $ lineCount file) ++ " "
+  putStr $ " " ++ (show $ lineCount file) ++ " "
   putStr $ (show $ wordCount file) ++ " "
   putStr $ (show $ charCount file) ++ " "
   putStrLn $ args !! 0 
